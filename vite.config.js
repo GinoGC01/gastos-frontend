0e3a591a82d9c646100de1eabc3391cf0de8e46b
Gino Ciancia <ginociancia10@gmail.com>
2025-12-12 17:49:35 -0300

    deploy netlify

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   build: {
+    outDir: 'dist',
+    // Opcional: mejora el build para Netlify
+    rollupOptions: {
+      output: {
+        manualChunks: undefined // Para un solo bundle o ajusta según necesidades
+      }
+    }
+  }
 })
